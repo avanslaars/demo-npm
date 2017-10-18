bf43ee22756cdd879dc8ae94121902aced6171fd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-18 10:17:17 -0400

    initial code

---
+++ src/demo-npm-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const demoNpm = require('.')
+
+describe('demo-npm', () => {
+  it('write this test', () => {
+    console.assert(demoNpm, 'should export something')
+  })
+})
